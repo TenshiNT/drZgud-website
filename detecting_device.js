ce7a95dd51032388435f59398920909e754444ec
TenshiNT <82107241+TenshiNT@users.noreply.github.com>
2023-01-13 08:27:56 +0100

    Add files via upload

--- detecting_device.js
+++ detecting_device.js
@@ -4,11 +4,6 @@ function detectMob() {
   }
 
 if (size == false) {
-  const strap = document.getElementsByClassName('strap');
-  strap.style.innerWidth = "100%";
-
-  const titleStrap = document.getElementsByClassName('titleLine');
-  titleStrap.style.innerWidth = '100%';
-  titleStrap.style.innerHeight = '1000px'
+  document.getElementById('stylesheet').href='styleMobile.css';
     
 }

e88932fb20f8f4307a01609414edfa9e5457abd9
TenshiNT <82107241+TenshiNT@users.noreply.github.com>
2023-01-13 08:21:00 +0100

    Add files via upload

---
+++ detecting_device.js
@@ -0,0 +1,13 @@
+function detectMob() {
+    let size = ( ( window.innerWidth <= 800 ) && ( window.innerHeight <= 600 ) );
+    return size;
+  }
+
+if (size == false) {
+  const strap = document.getElementsByClassName('strap');
+  strap.style.innerWidth = "100%";
+
+  const titleStrap = document.getElementsByClassName('titleLine');
+  titleStrap.style.innerWidth = '100%';
+    
+}

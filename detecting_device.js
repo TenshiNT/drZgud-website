7e868db326713d3d6ba30fa7e0da5e5d7d210ef0
TenshiNT <82107241+TenshiNT@users.noreply.github.com>
2023-01-13 08:32:33 +0100

    Add files via upload

--- detecting_device.js
+++ detecting_device.js
@@ -1,7 +1,5 @@
-function detectMob() {
-    let size = ( ( window.innerWidth <= 800 ) && ( window.innerHeight <= 600 ) );
-    return size;
-  }
+let size = ( ( window.innerWidth <= 800 ) && ( window.innerHeight <= 600 ) );
+return size;
 
 if (size == false) {
   document.getElementById('stylesheet').href='styleMobile.css';
